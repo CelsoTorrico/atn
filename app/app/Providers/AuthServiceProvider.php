46520c691a9698880e32c722be2a2643a4d64dd5
Iran Alves <iranjosealves@gmail.com>
2019-04-11 18:02:37 +0000

    Subindo base

--- app/app/Providers/AuthServiceProvider.php
+++ app/app/Providers/AuthServiceProvider.php
@@ -41,8 +41,6 @@ class AuthServiceProvider extends ServiceProvider
             //Carrega classe existente de logon anterior
             //$cookie = app('cookie')->queued(env('APPCOOKIE'));
 
-            var_dump($request);
-
             if ( !$request->hasCookie(env('APPCOOKIE')) ) {
                 $user = null;
             } else {
