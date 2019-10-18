9cf8d1d7972dd87308359dbce13f1aa8be8ed516
Iran Alves <iranjosealves@gmail.com>
2019-10-18 13:27:31 -0300

    Correção de cors para requisições via homepage atletasnow

--- app/app/Http/Middleware/Cors.php
+++ app/app/Http/Middleware/Cors.php
@@ -21,6 +21,8 @@ class Cors
 
         if(isset($_SERVER['HTTP_ORIGIN'])) {
             $http_origin = $_SERVER['HTTP_ORIGIN']; //Atribuindo servidor de requisição
+        } else {
+            $http_origin = $_SERVER['HTTP_REFERER'];
         }
         
         //Remove ponto inicial no domínio (se existir)
